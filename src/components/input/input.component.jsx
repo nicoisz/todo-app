a5964b370080f89d31de4fc0a31fdc6ba85bf880
nicoisz <nicolasignacio.sz@gmail.com>
2023-12-11 16:36:08 -0300

    correcciones en modal para full width

---
+++ src/components/input/input.component.jsx
@@ -0,0 +1,20 @@
+const CustomInput = () => {
+  return (
+    <div>
+      <label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900">
+        Name of task
+      </label>
+      <div className="relative mt-2 rounded-md shadow-sm w-full">
+        <input
+          type="text"
+          name="name"
+          id="name"
+          className="block w-full rounded-md border-0 py-1.5 pl-3 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          placeholder="Do Exercise"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default CustomInput;
