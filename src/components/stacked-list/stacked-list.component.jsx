30c484fcfc5bf3ccba76fde852273753d27a706b
nicoisz <nicolasignacio.sz@gmail.com>
2024-01-19 23:05:54 -0300

    separacion de elementos realizados y no realizados

--- src/components/stacked-list/stacked-list.component.jsx
+++ src/components/stacked-list/stacked-list.component.jsx
@@ -24,36 +24,84 @@ const StackedList = () => {
   };
 
   return (
-    <div>
-      <ul role="list" className="border-3 z-10 ">
-        {taskList.map((task) => (
-          <li key={task.id} className="flex justify-around gap-x-6 py-5 border bg-slate-300 ">
-            <div className="flex min-w-0 gap-x-4 border-red-600 border-dotted" onClick={() => handleDoneClick(task.id)}>
-              <div className="min-w-0 flex-auto">
-                <p className="text-sm font-semibold leading-6 text-gray-900">{task.name}</p>
-                {task.done ? <h1>Welcome back!</h1> : <h1>Please log in.</h1>}
-                <p className="mt-1 truncate text-xs leading-5 text-gray-500">{task.date}</p>
-              </div>
-            </div>
-            <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-              <div className="mt-1 flex items-center gap-x-1.5">
-                <button
-                  onClick={() => handleClickEdit(task)}
-                  className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+    <div className="flex flex-row justify-around flex-wrap">
+      <div className="w-full">
+        <h5 className="bg-blue-900">not done</h5>
+        <ul role="list" className="border-3 z-10 border">
+          {taskList.map((task) =>
+            task.done ? (
+              <li key={task.id} className="flex justify-around gap-x-6 py-5 border bg-slate-300 ">
+                <div
+                  className="flex min-w-0 gap-x-4 border-red-600 border-dotted"
+                  onClick={() => handleDoneClick(task.id)}
                 >
-                  <PencilIcon className="h-6 w-6 text-gray-600" aria-hidden="true" />
-                </button>
-                <button
-                  onClick={() => handleClickDelete(task)}
-                  className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+                  <div className="min-w-0 flex-auto">
+                    <p className="text-sm font-semibold leading-6 text-gray-900">{task.name}</p>
+                    <p className="mt-1 truncate text-xs leading-5 text-gray-500">{task.date}</p>
+                  </div>
+                </div>
+                <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+                  <div className="mt-1 flex items-center gap-x-1.5">
+                    <button
+                      onClick={() => handleClickEdit(task)}
+                      className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+                    >
+                      <PencilIcon className="h-6 w-6 text-gray-600" aria-hidden="true" />
+                    </button>
+                    <button
+                      onClick={() => handleClickDelete(task)}
+                      className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+                    >
+                      <TrashIcon className="h-6 w-6 text-gray-600" aria-hidden="true" />
+                    </button>
+                  </div>
+                </div>
+              </li>
+            ) : (
+              ""
+            )
+          )}
+        </ul>
+      </div>
+      <div className="w-full">
+        <h5> done</h5>
+        <ul role="list" className="border-3 z-10">
+          {taskList.map((task) =>
+            task.done == false ? (
+              <li key={task.id} className="flex justify-around gap-x-6 py-5 border bg-slate-300 ">
+                <div
+                  className="flex min-w-0 gap-x-4 border-red-600 border-dotted"
+                  onClick={() => handleDoneClick(task.id)}
                 >
-                  <TrashIcon className="h-6 w-6 text-gray-600" aria-hidden="true" />
-                </button>
-              </div>
-            </div>
-          </li>
-        ))}
-      </ul>
+                  <div className="min-w-0 flex-auto">
+                    <p className="text-sm font-semibold leading-6 text-gray-900">{task.name}</p>
+
+                    <p className="mt-1 truncate text-xs leading-5 text-gray-500">{task.date}</p>
+                  </div>
+                </div>
+                <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+                  <div className="mt-1 flex items-center gap-x-1.5">
+                    <button
+                      onClick={() => handleClickEdit(task)}
+                      className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+                    >
+                      <PencilIcon className="h-6 w-6 text-gray-600" aria-hidden="true" />
+                    </button>
+                    <button
+                      onClick={() => handleClickDelete(task)}
+                      className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+                    >
+                      <TrashIcon className="h-6 w-6 text-gray-600" aria-hidden="true" />
+                    </button>
+                  </div>
+                </div>
+              </li>
+            ) : (
+              ""
+            )
+          )}
+        </ul>
+      </div>
     </div>
   );
 };
